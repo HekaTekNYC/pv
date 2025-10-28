d335306abaaa1114227a3560cdf49a2cf13200d2
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-10-28 15:29:38 -0400

    styling(trustBanner): styling and media queries
    styling and responsiveness completed on trust cards as well as banner

---
+++ src/data/trust-data.ts
@@ -0,0 +1,23 @@
+export interface TrustItem {
+  icon: string;
+  alt: string;
+  text: string;
+}
+
+export const trustBannerData: TrustItem[] = [
+  {
+    icon: "/icons/contact-family.svg",
+    alt: "white outlined family icon",
+    text: "Family Owned",
+  },
+  {
+    icon: "/icons/contact-location.svg",
+    alt: "white outlined pin on map icon",
+    text: "Serving Northern Utah & Beyond",
+  },
+  {
+    icon: "/icons/contact-phone.svg",
+    alt: "white outlined phone icon",
+    text: "(801) 555-1234",
+  },
+];
