b08c938708a74212e1283393f04a940916869863
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-12-01 11:57:46 -0500

    chore(SEO): sitemap
    astro sitemap downloaded. sitemap corrections needed after domain connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
 import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [react()],
+  site: "https://www.perfectviewutah.com" || "http://localhost:4321",
+  // site: "https://www.perfectviewutah.com",
+  integrations: [sitemap()],
 });
