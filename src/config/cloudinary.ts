c01291e4d604b5d7cfff0160ce7c99eec32c1c61
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-12-01 13:49:49 -0500

    chore(lphero): background image
    reducing LCP

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -33,13 +33,6 @@ export const CLOUDINARY_BASE_AUTO =
  * getCloudinarySrc("about-hero", "webp"); // f_auto,q_auto optimized URL
  * ```
  */
-// export function getCloudinarySrc(
-//   file: string,
-//   ext: string = "png",
-//   version: string = "",
-// ): string {
-//   return `${CLOUDINARY_BASE_AUTO}/${version}/${file}.${ext}`;
-// }
 
 // config/cloudinary.ts - MINIMAL FIX TO REMOVE EXTRA SLASH
 export function getCloudinarySrc(
